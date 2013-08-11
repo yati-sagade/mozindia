ed23e79109f3fa256008505b9cbbf4f53cf43a09
Yati Sagade <yati.sagade@gmail.com>
2013-08-11 17:07:10 +0530

    Added bootstrap; initial design

--- mozindia/mainsite/urls.py
+++ mozindia/mainsite/urls.py
@@ -1,11 +1,17 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
+from .helpers.download_buttons import download_firefox
 
 from . import views
 
 
 urlpatterns = patterns('',
     url(r'^$', direct_to_template,
-        {'template': 'mainsite/home.html'},
+        {
+            'template': 'mainsite/home.html',
+            'extra_context': {
+                'download_firefox': lambda: download_firefox,
+            },
+        },
         name='mainsite.home'),
 )
